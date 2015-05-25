82d3f2a03e603817b423183bd528135edd4238e8
Caleb Doise <calebdoise@users.noreply.github.com>
2015-05-25 01:16:59 -0400

    start of work on SQL entry store

---
+++ src/WinterLockbox/WinterLockbox.Sql/SqlLockboxEntryStoreMigrator.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace WinterLockbox.EntryStores
+{
+    public class SqlLockboxEntryStoreMigrator
+    {
+        private string connectionString;
+
+        public SqlLockboxEntryStoreMigrator(string connectionString)
+        {
+            this.connectionString = connectionString;     
+        }       
+ 
+        public void MigrateToLatest()
+        {            
+            using (TransactionScope scope = new TransactionScope())
+            using (SqlConnection connection = new SqlConnection(this.connectionString))
+            {
+                connection.Open();
+
+                long currentVersion = -1;
+                try
+                {
+                    using (var cmd = connection.CreateCommand())
+                    {
+                        cmd.CommandText = "SELECT MAX(VersionNumber) FROM Version;";
+
+                        object currentVersionObject = cmd.ExecuteScalar();
+                        if (currentVersionObject != null)
+                        {
+                            currentVersion = Convert.ToInt64(currentVersionObject);
+                        }
+                    }
+                }
+                catch
+                {
+                }
+
+                if (currentVersion < 1)
+                {
+                    Up_Migration_1(connection);
+                }
+            }            
+        }
+
+        private void Up_Migration_1(SqlConnection connection)
+        {
+            using (var cmd = connection.CreateCommand())
+            {
+                cmd.CommandText = @"CREATE TABLE Version (VersionNumber bigint NOT NULL PRIMARY KEY, MigratedAt datetime NOT NULL);";
+                cmd.ExecuteNonQuery();
+            }
+
+            using (var cmd = connection.CreateCommand())
+            {
+                cmd.CommandText = @"
+CREATE TABLE LockboxRepository(
+    Id int NOT NULL PRIMARY KEY,
+    Name nvarchar(255)
+);
+
+CREATE TABLE LockboxEntry(
+    RepositoryId int NOT NULL,
+    EntryGuid varchar(255) NOT NULL,
+    KeyHash varchar(512) NOT NULL,
+    EncryptedKeyBytes varbinary(max),
+    KeyIV varbinary(max),  
+    EncryptedValueBytes varbinary(max),  
+    ValueIV varbinary(max),
+    ValueType int NOT NULL,
+
+    PRIMARY KEY 
+    (
+	    RepositoryId, 
+        EntryGuid
+    )
+);
+
+CREATE INDEX IX_LockboxEntry_KeyHash ON LockboxEntry (KeyHash);
+";
+                cmd.ExecuteNonQuery();
+            }
+
+            AddMigrationVersionNumber(connection, 1);
+        }
+
+        private void AddMigrationVersionNumber(SqlConnection connection, long versionNumber)
+        {
+            using (var cmd = connection.CreateCommand())
+            {
+                cmd.CommandText = @"INSERT INTO Version (VersionNumber, MigratedAt) VALUES (@versionNumber, GETUTCDATE());";
+                cmd.Parameters.AddWithValue("versionNumber", versionNumber);
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
--- src/WinterLockbox/WinterLockboxUnitTests/KeyValueLockboxTests.cs
+++ src/WinterLockbox/WinterLockboxUnitTests/KeyValueLockboxTests.cs
@@ -3,25 +3,86 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using WinterLockbox.InMem;
 using FlowBasis.Crypto;
 using WinterLockbox;
+using WinterLockbox.EntryStores;
+using System.Data.SqlClient;
+using System.IO;
 
 namespace WinterLockboxUnitTests
 {
     [TestClass]
     public class KeyValueLockboxTests
-    {
+    {        
+        [ClassInitialize]
+        public static void SetUp(TestContext context)
+        {
+            AppDomain.CurrentDomain.SetData(
+                "DataDirectory",
+                context.TestDeploymentDir);
+        }
+
         [TestMethod]
-        public void Test_Basic_Entry_Manipulation()
+        public void Test_Basic_Entry_Manipulation_InMem()
         {
             var sharedKey = SymmetricKey.FromPassword(SymmetricKeyType.AES, "my shared key", 256);
             var entryStore = new InMemLockboxEntryStore();
 
-            var lockbox = new KeyValueLockbox(
+            this.Test_Basic_Operations_With_Options(
                 new KeyValueLockboxOptions
                 {
                     GlobalSalt = SecureRandomBytes.GetRandomBytes(32),
                     SymmetricKey = sharedKey,
                     EntryStore = entryStore
-                });                
+                });
+        }
+
+        
+        [TestMethod]
+        public void Test_Basic_Entry_Manipulation_Sql()
+        {
+            string dataDir = AppDomain.CurrentDomain.GetData("DataDirectory") as string;
+            string dbPath = Path.Combine(dataDir, "LockboxUnitTest.mdf");
+            string dbName = "LockboxUnitTest";
+
+            using (var connection = new SqlConnection(@"Data Source=(LocalDb)\v11.0;Initial Catalog=Master;Integrated Security=True"))
+            {
+                connection.Open();
+
+                using (var cmd = connection.CreateCommand())
+                {                    
+                    cmd.CommandText = @"
+                        IF EXISTS(SELECT * FROM sys.databases WHERE name = @dbName)
+                        BEGIN
+	                        ALTER DATABASE [" + dbName + @"]
+	                        SET SINGLE_USER
+	                        WITH ROLLBACK IMMEDIATE
+	                        DROP DATABASE [" + dbName + @"]
+                        END
+             
+                        EXEC ('CREATE DATABASE [' + @dbName + '] ON PRIMARY 
+	                        (NAME = [' + @dbName + '], 
+	                        FILENAME =''' + @filename + ''', 
+	                        SIZE = 25MB, 
+	                        MAXSIZE = 50MB, 
+	                        FILEGROWTH = 5MB )')";
+
+                    cmd.Parameters.AddWithValue("dbName", dbName);
+                    cmd.Parameters.AddWithValue("filename", dbPath);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+            string connectionString =
+                @"Data Source=(LocalDb)\v11.0;Initial Catalog=LockboxUnitTest;Integrated Security=SSPI;"
+                + @"AttachDBFilename=|DataDirectory|\LockboxUnitTest.mdf";
+
+            SqlLockboxEntryStoreMigrator sqlMigrator = new SqlLockboxEntryStoreMigrator(connectionString);
+            sqlMigrator.MigrateToLatest();
+        }
+
+        private void Test_Basic_Operations_With_Options(KeyValueLockboxOptions lockboxOptions)
+        {
+            var lockbox = new KeyValueLockbox(lockboxOptions);
 
             lockbox.SetEntry("Hello", "World");
             lockbox.SetEntry("Blarg", "42");
@@ -39,13 +100,13 @@ namespace WinterLockboxUnitTests
 
             lockbox.DeleteEntry("Blarg");
 
-            Assert.AreEqual("World", lockbox.GetEntryValueString("Hello"));            
+            Assert.AreEqual("World", lockbox.GetEntryValueString("Hello"));
             Assert.AreEqual("this is the value to be encrypted", lockbox.GetEntryValueString("Some other key"));
 
             Exception caughtEx = null;
             try
             {
-                lockbox.GetEntryValueString("Blarg");                
+                lockbox.GetEntryValueString("Blarg");
             }
             catch (Exception ex)
             {
@@ -56,7 +117,7 @@ namespace WinterLockboxUnitTests
 
             keyList = lockbox.GetKeyList();
             Assert.AreEqual(2, keyList.Count);
-            Assert.IsTrue(keyList.Contains("Hello"));            
+            Assert.IsTrue(keyList.Contains("Hello"));
             Assert.IsTrue(keyList.Contains("Some other key"));
 
             lockbox.SetEntry("blob data", new byte[] { 2, 4, 10, 42 });
